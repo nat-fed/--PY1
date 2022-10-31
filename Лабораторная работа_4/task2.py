90652067e4caa6ac0e31b5f00e9a703d3ca34b89
nat-fed <115354317+nat-fed@users.noreply.github.com>
2022-10-31 22:09:25 +0300

    Update task2.py

--- Лабораторная работа_4/task2.py
+++ Лабораторная работа_4/task2.py
@@ -18,3 +18,14 @@ main_str = """
     Далее нужно отсортировать слова в алфавитном порядке, а после сортировки склеить их с помощью метода строк join. Приступим!!!!
 """
 print(get_count_char(main_str))
+
+def get_count_char_percent(dict_):
+    total = sum(dict_.values())
+    simbol_percent = {}
+    for simbol, count in dict_.items():
+        percent = round(count / total * 100, 2)
+        simbol_percent[simbol] = simbol_percent.get(simbol, percent)
+
+    return simbol_percent
+
+print(get_count_char_percent(get_count_char(main_str)))
