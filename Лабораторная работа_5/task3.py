56ff8d02ec6cca3dba6435d7838e03d980f25bcb
nat-fed <115354317+nat-fed@users.noreply.github.com>
2022-11-04 18:16:16 +0300

    Update task3.py

--- Лабораторная работа_5/task3.py
+++ Лабораторная работа_5/task3.py
@@ -20,7 +20,6 @@ def get_unique_list_numbers() -> list[int]:
     return list_numbers
     # TODO написать функцию для получения списка уникальных целых чисел
 
-
 list_unique_numbers = get_unique_list_numbers()
 print(list_unique_numbers)
 print(len(list_unique_numbers) == len(set(list_unique_numbers)))
