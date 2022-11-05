6fd7653645c6076f1395001158bc9db3fb1b8d7c
nat-fed <115354317+nat-fed@users.noreply.github.com>
2022-11-05 21:26:45 +0300

    Update task5.py

--- Лабораторная работа_5/task5.py
+++ Лабораторная работа_5/task5.py
@@ -1,16 +1,17 @@
 from random import sample
 import string
 
-def get_random_password() -> str:
+def get_random_password(n=None) -> str:
     uppercase_char = string.ascii_uppercase
     lowercase_char = string.ascii_lowercase
     digits = string.digits
     all_char = uppercase_char + lowercase_char + digits
 
-    n = 8
-    password = sample(all_char, n)
+    if n is None:
+        password = sample(all_char, 8)
+    else:
+        password = sample(all_char, n)
 
     return "".join(password)
-    # TODO написать функцию генерации случайных паролей
 
 print(get_random_password())
