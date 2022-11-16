613959c03fcb76800d956e0f1e0358aaa8e9e05f
nat-fed <115354317+nat-fed@users.noreply.github.com>
2022-11-16 18:59:38 +0300

    Update task1.py

--- Лабораторная работа_5/task1.py
+++ Лабораторная работа_5/task1.py
@@ -1,7 +1,6 @@
-# TODO решить с помощью list comprehension и распечатать его
+from pprint import pprint
+
 stop = 15
 list_ = [{"bin": bin(num), "dec": num, "hex": hex(num), "oct": oct(num)} for num in range(stop + 1)]
-
-from pprint import pprint
 
 pprint(list_)
